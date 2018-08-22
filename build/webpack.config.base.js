dd8a06654889fe18c3d169d7e82e2c504faca1a0
pengteling <pengteling@gmail.com>
2018-08-22 20:06:49 +0800

    merge

---
+++ build/webpack.config.base.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const isDev = process.env.NODE_ENV === "development";
+const createVueLoaderConfig = require("./vue-loader.config");
+
+// console.log(isDev)
+const config = {
+  entry: path.resolve(__dirname, "../src/index.js"),
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist")
+  },
+  resolve: {
+    extensions: ["*", ".vue", ".js", ".jsx"],
+    alias: {
+      vue$: "vue/dist/vue.common.js",
+      "@": path.resolve(__dirname, "../src/components")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: createVueLoaderConfig(isDev)
+        // options: {
+        //   //提取vue文件中的样式
+        //   extractCSS: !isDev
+        // }
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1024,
+              name: "[name]-[hash:base64:8].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
