e2d764f3a6ad9e6d5ec766464f4a905d6e28d74f
pengteling <pengteling@gmail.com>
2018-08-29 18:42:16 +0800

    ssr

---
+++ build/webpack.conifg.server.js
@@ -0,0 +1,121 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isDev = process.env.NODE_ENV === "development";
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+// console.log(isDev)
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.config.base");
+
+let config;
+const defaultOptions = {
+  plugins: [
+
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: isDev
+          ? JSON.stringify("development")
+          : JSON.stringify("production")
+      }
+    })
+  ]
+};
+
+if (isDev) {
+  //开发者模式
+
+  target: 'node',
+  config = merge(baseConfig, defaultOptions, {
+    devtool: "source-map",
+    entry:
+       path.resolve(__dirname, "../src/server-entry.js"),
+    output:{
+      libraryTarget:'commonjs2',
+    },
+
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                // camelCase: true
+                localIdentName: "[path]-[name]-[hash:base64:5]",
+                sourceMap: true
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  });
+} else {
+  //生产模式
+  config = merge(baseConfig, defaultOptions, {
+    entry: {
+      app: path.resolve(__dirname, "../src/index.js"),
+      vendor: ["vue"]
+    },
+    output: {
+      filename: "[name]-[chunkhash:8].js"
+    },
+    plugins: [
+      new ExtractTextWebpackPlugin("style-[contentHash:8].css"),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor"
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "runtime"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ExtractTextWebpackPlugin.extract({
+            fallback: "style-loader",
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    }
+  });
+}
+
+module.exports = config;
