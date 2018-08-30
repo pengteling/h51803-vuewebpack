287fded00a2d35eba57376f9f4848b9675a2ff85
pengteling <pengteling@gmail.com>
2018-08-30 16:10:43 +0800

    0830

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ import player from './player'
 
 Vue.use(vuex)
 
-export default new vuex.Store({
+const store = new vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   modules: {
     list,
     player
   }
 })
+// 加上热更替功能
+if (module.hot) {
+  module.hot.accept([
+    './list',
+    './player'
+  ], () => {
+    const newModuleList = require('./list').default
+    const newModulePlayer = require('./player').default
+    store.hotUpdate({
+      modules: {
+        list: newModuleList,
+        player: newModulePlayer
+      }
+    })
+  })
+}
+export default store
